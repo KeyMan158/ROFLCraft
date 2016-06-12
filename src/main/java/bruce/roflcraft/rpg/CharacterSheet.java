7bbb5c33ca515cdf891d14692fc64386945cf980
KeyMan158 <rich.p.key@gmail.com>
2016-06-12 23:09:36 +0100

    Added attributes enum and  character sheet.

--- src/main/java/bruce/roflcraft/rpg/CharacterSheet.java
+++ src/main/java/bruce/roflcraft/rpg/CharacterSheet.java
@@ -2,5 +2,17 @@ package bruce.roflcraft.rpg;
 
 public class CharacterSheet 
 {
+	private String name;
+	private Skill skills[];
+	private Attribute attributes[];
 	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String characterName)
+	{
+		name = characterName;
+	}
 }
---
+++ src/main/java/bruce/roflcraft/settings/Attributes.java
@@ -0,0 +1,5 @@
+package bruce.roflcraft.settings;
+
+public enum Attributes {
+	STRENGTH,DEXTERITY,CONSTITUTION,INTELLIGENCE,WISDOM,CHARISMA
+}
