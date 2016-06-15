ea2bf7dc2906218a16c7342cf33489b44d866355
Lorrtath <lee.bill@hotmail.co.uk>
2016-06-15 21:33:24 +0100

    GUI Test Texture
    Assets extended for GUI and example added

--- src/main/java/bruce/roflcraft/gui/RoflGUIScreen.java
+++ src/main/java/bruce/roflcraft/gui/RoflGUIScreen.java
@@ -1,5 +1,8 @@
 package bruce.roflcraft.gui;
 
+import org.lwjgl.opengl.GL11;
+
+import bruce.roflcraft.main.Reference;
 import bruce.roflcraft.settings.GUIIDs;
 
 //========================================
@@ -9,22 +12,33 @@ import bruce.roflcraft.settings.GUIIDs;
 //========================================
 
 import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiControls;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.util.ResourceLocation;
 
 public class RoflGUIScreen extends GuiScreen
 {
+	//default width = 480
+	//default height = 255
+	
 	//public static final int GUIID = GUIIDs.CharacterSheet.ordinal();
 	
 	//Test GUI buttons (should be private...)
 	public GuiButton a;
 	public GuiButton b;
-		
+	
+	private final String TEXTURENAME = "CharacterSheet.png";
+	
+
 	@Override
 	public void drawScreen(int mouseX, int mouseY, float partialTicks)
 	{
 		//This method draws GUI content to the screen
 		
 		this.drawDefaultBackground();//Set the background to the default value
+		this.mc.getTextureManager().bindTexture(new ResourceLocation(Reference.MODID + ":textures/gui/" + TEXTURENAME));//Use this to load in a 256x256 texture
+		//This draws a rectangle using the loaded texture.(left, top, texture U min, texture V Min, texture U Maz, texture V Max)
+		this.drawTexturedModalRect(0, 0, 0, 0,100, 200);
 		super.drawScreen(mouseX, mouseY, partialTicks);
 	}
 	
@@ -42,6 +56,7 @@ public class RoflGUIScreen extends GuiScreen
 		
 		//default size is 200x20. With default texture resources, 20x20 should be the smallest 
 		//there is also a constructor overload with width & height
+
 		a = new GuiButton(0, this.width/2 - 100, this.height / 2 - 24, "This is button 'A'");
 		b = new GuiButton(1, this.width/2 - 100, this.height / 2 + 4, "This is not button 'A'");
 		
